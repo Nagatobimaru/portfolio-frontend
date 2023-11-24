4c5927a9fe651ce2d932a5ccf57e62ec02e1a19d
phuc-nguyen-saltovn <phuc.nguyen@salto.vn>
2023-11-24 15:53:34 +0700

    feat: disabled pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
+import ViteYaml from '@modyfi/vite-plugin-yaml';
+import {esbuildCommonjs} from '@originjs/vite-plugin-commonjs';
+import {defineNuxtConfig} from 'nuxt/config';
+import viteCompression from 'vite-plugin-compression';
+import ViteCSSExportPlugin from 'vite-plugin-css-export';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@kevinmarrec/nuxt-pwa'],
+
   app: {
     head: {
       titleTemplate: '%s - WHITEBEAR',
@@ -42,14 +50,27 @@ export default defineNuxtConfig({
     },
   },
 
-  modules: ['@kevinmarrec/nuxt-pwa'],
-
-  pwa: {
-    manifest: {
-      crossorigin: 'use-credentials',
-      start_url: '/',
-      icon: {
-        sizes: [16, 32, 64, 144, 192, 384, 512],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/variables.scss";',
+        },
+      },
+    },
+    define: {
+      'process.env.DEBUG': false,
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        // https://github.com/jaydenseric/apollo-upload-client/issues/280#issuecomment-1363605321
+        plugins: [esbuildCommonjs(['apollo-upload-client'])],
+      },
+    },
+    plugins: [ViteYaml(), ViteCSSExportPlugin(), viteCompression()],
+    server: {
+      hmr: {
+        overlay: false,
       },
     },
   },
